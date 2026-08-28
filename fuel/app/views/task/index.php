029e583cd62cf381c58eed1c2afadd2c110ecab1
supu1495 <xyz137997315@icloud.com>
2026-08-29 01:13:12 +0900

    タスク一覧を掲示板レイアウト（2ペイン・タスク表示）にする

--- fuel/app/views/task/index.php
+++ fuel/app/views/task/index.php
@@ -1,5 +1,29 @@
-<ul>
-    <?php foreach ($tasks as $task): ?>
-        <li><?php echo $task['title']; ?></li>
-    <?php endforeach; ?>
-</ul>
+<div class="grid">
+    <div class="pane"><!-- 左の箱 -->
+        <div class="pane-h"><!-- タスク一覧 -->
+            タスク一覧(<?php echo count($tasks); ?>件)
+        </div>
+        <div class="board"><!-- カードを並べる箱 -->
+            <?php foreach ($tasks as $task): ?>
+                <div class="task" style="border-left-color: <?php echo $task['tag_color']; ?>">
+                    <div class="t-top">
+                        <div class="check"></div>
+                        <div class="t-body">
+                            <div class="t-titlerow">
+                                <span class="t-title"><?php echo $task['title']; ?></span>
+                                <span class="tag" style="background: <?php echo $task['tag_color']; ?>"><?php echo $task['tag_name']; ?></span>
+                                <span class="due">締切<?php echo $task['deadline']; ?></span>
+                            </div>
+                            <?php if ($task['memo'] !== '') : ?>
+                                <div class="t-memo">📝 <?php echo $task['memo']; ?></div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <div class="pane">
+        登録フォーム（準備中）
+    </div>
+</div>
--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,10 +2,16 @@
  <html lang="ja">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>家庭内タスク掲示板</title>
         <?php echo Asset::css('board.css'); ?>
     </head>
     <body>
-        <?php echo $content; ?>
+        <div class="wrap">
+            <div class="appbar">
+                <div class="brand">家庭内タスク掲示板</div>
+            </div>
+            <?php echo $content; ?>
+        </div>
     </body>
-</html>
+ </html>
