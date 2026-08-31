8a55345d461cb2cf31b6a4142f636a872e7238fc
supu1495 <xyz137997315@icloud.com>
2026-08-31 23:01:21 +0900

    タスクの完了状態を切り替えてJSONを返すアクションを追加する

--- fuel/app/classes/controller/task.php
+++ fuel/app/classes/controller/task.php
@@ -116,6 +116,18 @@ class Controller_Task extends Controller_Template{
         Response::redirect('task/edit/'.$task_id);
     }
 
+    public function action_toggle(){
+        $id = Input::post('id');
+        if (! ctype_digit($id)){
+            return $this->json_response(array('error' => 'id is bad'), 400);
+        }
+        if (\Model\Task::toggle_done($id) === 0){
+            return $this->json_response(array('error' => 'id not found'), 404);
+        }
+        $task = \Model\Task::find_by_id($id);
+        return $this->json_response(array('id' => (int)$id, 'done' => (int)$task['done']));
+    }
+
     private function attach_subtasks($tasks){
         $ids = array_column($tasks, 'id');
         $subtasks = \Model\SubTask::find_by_task_ids($ids);
@@ -163,5 +175,9 @@ class Controller_Task extends Controller_Template{
             }
         }
         return $tasks;
+    }
+
+    private function json_response($data, $status = 200){
+        return new Response(json_encode($data), $status, array('Content-Type' => 'application/json'));
     }
 }
